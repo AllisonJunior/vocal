897c9eb3ebac64a9853f8b55c9d30e0b21ff8a38
Allen <114815898+AllisonJunior@users.noreply.github.com>
2025-04-08 12:31:42 -0300

    login check

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,8 @@ document.querySelector(".main_login_form").addEventListener("submit", async (e) 
   const data = await res.json();
 
   if (res.ok && data.success) {
-    alert("Login realizado com sucesso!");
+    localStorage.setItem("user", user); // salva o usuário localmente
+    window.location.href = "/pages/chat.html"; // redireciona para o chat
   } else {
     alert("Usuário ou senha incorretos.");
   }
